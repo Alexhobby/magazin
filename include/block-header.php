d584d651ae56473e0f53851d9e9ab8988d9002a4
Alexandr <i9312452497@gmail.com>
2020-06-25 00:26:48 +0300

    menu

--- include/block-header.php
+++ include/block-header.php
@@ -8,5 +8,9 @@
     <li><a href="contacts.php">Контакты</a></li>
 </ul>
 
+
+<p id="reg-auth-title" align="right"><a class="top-auth">Вход</a><a href="registr.php">Регистрация</a></p>
+
 </div>
+<div id="top-line"></div>
 </div>
